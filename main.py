f1838e931209108821434b1291af6155f7c96fa8
savantaakash322@gmail.com <savantaakash322@gmail.com>
2023-09-16 15:03:40 +0530

    Remainder functionality added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import sqlite3
+import datetime
+from plyer import notification
+import time
+import threading
 
 # Create a connection to the database or create it if it doesn't exist
 conn = sqlite3.connect('todo.db')
@@ -10,7 +14,8 @@ cursor = conn.cursor()
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY,
-        task TEXT NOT NULL
+        task TEXT NOT NULL,
+        reminder_time TEXT
     )
 ''')
 
@@ -19,12 +24,16 @@ conn.commit()
 conn.close()
 
 def create_task():
-    task = input("Enter the task to create : ")
+    task = input("Enter the task to create: ")
+    reminder_time = input("Enter the reminder time (HH:MM): ")
+
     conn = sqlite3.connect('todo.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO tasks (task) VALUES (?)', (task,))
+    cursor.execute('INSERT INTO tasks (task, reminder_time) VALUES (?, ?)', (task, reminder_time))
     conn.commit()
     conn.close()
+
+    print(f"Task added to the to-do list with a reminder at {reminder_time}.")
 
 def read_tasks():
     conn = sqlite3.connect('todo.db')
@@ -47,8 +56,38 @@ def delete_task(task_id):
     cursor.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
     conn.commit()
     conn.close()
+    
+    
+def check_reminders():
+    while True:
+        conn = sqlite3.connect('todo.db')
+        cursor = conn.cursor()
+        now = datetime.datetime.now().strftime('%H:%M')
+
+        # Fetch tasks with reminders
+        cursor.execute('SELECT * FROM tasks WHERE reminder_time = ?', (now,))
+        tasks = cursor.fetchall()
+        conn.close()
+
+        if tasks:
+            for task in tasks:
+                notification_title = "Task Reminder"
+                notification_message = task[1]
+                notification_timeout = 10  # Notification duration in seconds
+                notification.notify(
+                    title=notification_title,
+                    message=notification_message,
+                    timeout=notification_timeout
+                )
+
+        # Sleep for a while before checking again (e.g., every minute)
+        time.sleep(60)
 
 def main():
+    
+    reminder_thread = threading.Thread(target=check_reminders)
+    reminder_thread.daemon = True  # This allows the thread to exit when the main program exits
+    reminder_thread.start()
     while True:
         print("\nTo-Do List Menu:")
         print("1. Add a task")
@@ -62,7 +101,8 @@ def main():
         if choice == "1":
             create_task()
             print("Task added to the to-do list.")
-
+            check_reminders()
+            
         elif choice == "2":
             tasks = read_tasks()
             print("\nTo-Do List:")
@@ -81,7 +121,7 @@ def main():
             print("Task deleted.")
 
         elif choice == "5":
-            print("Thanks for to do application!")
+            print("Thanks for using the application!")
             break
 
 if __name__ == "__main__":
